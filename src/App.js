e35407e26bb8f720a274080618152059437145d2
Mario Souto <soutomarios@gmail.com>
2018-02-08 15:36:39 -0200

    adicionando fragment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Cabecalho from './components/Cabecalho'
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Fragment>
         <Cabecalho usuario="@omariosouto" />
-      </div>
+      </Fragment>
     );
   }
 }
